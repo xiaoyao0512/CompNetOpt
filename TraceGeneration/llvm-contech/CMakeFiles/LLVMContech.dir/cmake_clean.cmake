7537b3ed8e9c67d8649fd317211e82f0014a67a0
Yao Xiao <xiaoyao@usc.edu>
2019-05-07 11:24:36 -0700

    Add files via upload

---
+++ TraceGeneration/llvm-contech/CMakeFiles/LLVMContech.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LLVMContech.dir/Contech.o"
+  "lib/LLVMContech.pdb"
+  "lib/LLVMContech.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LLVMContech.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
